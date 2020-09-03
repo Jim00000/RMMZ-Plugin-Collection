4bf14561097f862e99a2a7ef97a5f9f1c3a86cef
Jim00000 <good0121good@gmail.com>
2020-09-03 13:11:37 +0800

    Upload JPC_QuickSaveAndLoad.js

---
+++ JPC_QuickSaveAndLoad.js
@@ -0,0 +1,140 @@
+//=============================================================================
+// RPG Maker MZ - Quick Save/Load
+//=============================================================================
+
+/*:
+ * @target MZ
+ * @plugindesc Quick save/load mechanism.
+ * @author Jim00000
+ * @help 
+ * Use F6 to quicksave, F7 to quickload.
+ * 
+ * @param QuickSaveSlotId
+ * @text The target save slot which quick save/load picks
+ * @type number
+ * @default 0
+ * 
+ * @param HasFadeOutEffect
+ * @text Fade out while save file is loading
+ * @type boolean
+ * @default true
+ * @on Enable 
+ * @off Disable
+ */
+{
+    // Fixed variables
+    const PLUGIN_NAME = "jpc_quicksaveandload";
+    const QUICK_SAVE_KEY_STRING = "QuickSave";
+    const QUICK_LOAD_KEY_STRING = "QuickLoad";
+    const VK_F6 = 0x75;
+    const VK_F7 = 0x76;
+
+    // User-defined variables
+    const QUICK_SAVE_SLOT_ID = parseInt(GetPluginParams()['QuickSaveSlotId']);
+    const FADE_OUT_EFFECT = JSON.parse(GetPluginParams()['HasFadeOutEffect']);
+
+    function RegisterKeyBind(vkey, keyName) {
+        Input.keyMapper[vkey] = keyName;
+    }
+
+    function GetPluginParams() {
+        return PluginManager.parameters(PLUGIN_NAME);
+    }
+
+    function QuickSave(savefileId) {
+        if (savefileId >= 0 && savefileId < DataManager.maxSavefiles()) {
+            $gameSystem.setSavefileId(savefileId);
+            $gameSystem.onBeforeSave();
+            DataManager.saveGame(savefileId)
+                .then(() => { SoundManager.playSave(); })
+                .catch(() => { SoundManager.playBuzzer(); });
+        } else {
+            console.error("Cannot save file: id = %d", savefileId);
+            SoundManager.playBuzzer();
+        }
+    }
+
+    function QuickLoad(savefileId) {
+        if (savefileId >= 0 && savefileId < DataManager.maxSavefiles()) {
+            DataManager.loadGame(savefileId)
+                .then(() => {
+                    SoundManager.playLoad();
+                    if (FADE_OUT_EFFECT) {
+                        $gameScreen._fadeOutDuration = 50;
+                        setTimeout(function () {
+                            $gameScreen._fadeInDuration = 50;
+                            SceneManager.goto(Scene_Map);
+                        }, 500);
+                    } else {
+                        SceneManager.goto(Scene_Map);
+                    }
+                })
+                .catch(() => { SoundManager.playBuzzer(); });
+        } else {
+            console.error("Cannot load file: id = %d", savefileId);
+            SoundManager.playBuzzer();
+        }
+    }
+
+    function UpdateCallQuickSave() {
+        if (IsQuickSaveCalled()) {
+            QuickSave(QUICK_SAVE_SLOT_ID);
+        }
+    }
+
+    function UpdateCallQuickLoad() {
+        if (IsQuickLoadCalled()) {
+            QuickLoad(QUICK_SAVE_SLOT_ID);
+        }
+    }
+
+    function IsQuickSaveCalled() {
+        return Input.isTriggered(QUICK_SAVE_KEY_STRING);
+    }
+
+    function IsQuickLoadCalled() {
+        return Input.isTriggered(QUICK_LOAD_KEY_STRING);
+    }
+
+    (() => {
+        // Register F6 as quicksave hotkey
+        RegisterKeyBind(VK_F6, QUICK_SAVE_KEY_STRING);
+        // Register F7 as quickload hotkey
+        RegisterKeyBind(VK_F7, QUICK_LOAD_KEY_STRING);
+
+        const _Scene_Map__updateScene = Scene_Map.prototype.updateScene;
+        Scene_Map.prototype.updateScene = function () {
+            _Scene_Map__updateScene.apply(this, arguments);
+            if (!SceneManager.isSceneChanging()) {
+                UpdateCallQuickSave();
+            }
+            if (!SceneManager.isSceneChanging()) {
+                UpdateCallQuickLoad();
+            }
+        };
+
+    })();
+}
+
+/* MIT License
+
+Copyright (c) 2020 Jim00000
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
