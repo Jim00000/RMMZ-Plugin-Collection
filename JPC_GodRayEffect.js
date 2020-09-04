26d672c5994cc4049bfff2b2a0d1f64646d513b3
Jim00000 <good0121good@gmail.com>
2020-09-04 15:21:55 +0800

    Support getting angle from xml

--- JPC_GodRayEffect.js
+++ JPC_GodRayEffect.js
@@ -131,7 +131,9 @@
         this.isGodRayFilterApplied = JPC_ParseNoteToBoolean($dataMap.note, "godrayeffect.enable");
         if (this.isGodRayFilterApplied) {
             this.godRayFilterDelta = JPC_ParseNoteToFloat($dataMap.note, "godrayeffect.delta") || DELTA;
-            this.godRayFilter = CreateGodRayShader(ANGLE, GAIN, LACUNRITY, IS_LIGHT_PARALLEL, CENTER);
+            this.godRayFilter = CreateGodRayShader(
+                JPC_ParseNoteToInt($dataMap.note, "godrayeffect.angle") || ANGLE, 
+                GAIN, LACUNRITY, IS_LIGHT_PARALLEL, CENTER);
             this.filters.push(this.godRayFilter);
             this.godRayFilterUpdateHandler = UpdateGodRayShader;
         }
