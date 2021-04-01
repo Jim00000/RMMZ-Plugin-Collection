cf326bfb1afd3d5e073692a82221dd1daa341e9f
Jim00000 <good0121good@gmail.com>
2021-04-01 22:46:37 +0800

    Add utf8ToWchar and wcharToUtf8 funcs
    - new func: utf8ToWchar
    - new func: wcharToUtf8
    - replace by new funcs

--- jpc/native/textbox/src/binding.cpp
+++ jpc/native/textbox/src/binding.cpp
@@ -4,22 +4,32 @@
 
 namespace {
 
+    const std::wstring utf8ToWchar(const std::string& utf8) 
+    {
+        std::wstring wchar(utf8.size(), 0);
+        MultiByteToWideChar(CP_UTF8, 0, utf8.c_str(), utf8.size(), const_cast<LPWSTR>(wchar.c_str()), wchar.size());
+        return wchar;
+    }
+
+    const std::string wcharToUtf8(const std::wstring wstr) 
+    {
+        const int needed_sz = WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), wstr.size(), NULL, 0, NULL, NULL);
+        std::string utf8(needed_sz, 0);
+        WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), wstr.size(), const_cast<LPSTR>(utf8.c_str()), utf8.size(), NULL, NULL);
+        return utf8;
+    }
+
     Napi::String registerOpenTextBox(const Napi::CallbackInfo& info)
     {
         Napi::Env env = info.Env();
-        std::wstring text, title;
+        std::wstring wOutputText;
         const uint32_t win_x = info[0].As<Napi::Number>().Int32Value();
         const uint32_t win_y = info[1].As<Napi::Number>().Int32Value();
         const uint32_t width = info[2].As<Napi::Number>().Int32Value();
         const uint32_t height = info[3].As<Napi::Number>().Int32Value();
         const std::string utf8_title = info[4].As<Napi::String>().Utf8Value();
-        title.resize(utf8_title.size());
-        MultiByteToWideChar(CP_UTF8, 0, utf8_title.c_str(), utf8_title.size(), (LPWSTR)title.c_str(), title.size());
-        JPC::TextBox::open(title.c_str(), text, win_x, win_y, width, height);
-        const int needed_sz = WideCharToMultiByte(CP_UTF8, 0, text.c_str(), text.size(), NULL, 0, NULL, NULL);
-        std::string utf8_text(needed_sz, 0);
-        WideCharToMultiByte(CP_UTF8, 0, text.c_str(), text.size(), (LPSTR)utf8_text.c_str(), utf8_text.size(), NULL, NULL);
-        return Napi::String::New(env, utf8_text);
+        JPC::TextBox::open(utf8ToWchar(utf8_title).c_str(), wOutputText, win_x, win_y, width, height);
+        return Napi::String::New(env, wcharToUtf8(wOutputText));
     }
 
     Napi::Object Init(Napi::Env env, Napi::Object exports)
