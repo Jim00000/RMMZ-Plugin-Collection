d69855b52cd045b2531286126d70057fdac13201
Jim00000 <good0121good@gmail.com>
2021-04-03 21:48:23 +0800

    filter notetag

--- jpc/core/xmlparser.js
+++ jpc/core/xmlparser.js
@@ -1,9 +1,16 @@
 export const __xmlparser = {};
 
 __xmlparser.parse = function(text) {
+    const jpcxml = __xmlparser.filter(text);
     const parser = new DOMParser();
-    const xmldoc = parser.parseFromString(text, "text/xml");
+    const xmldoc = parser.parseFromString(jpcxml, "text/xml");
     return xmldoc;
+}
+
+__xmlparser.filter = function(text) {
+    const re = /(^<jpc>[\w\s\<\>\_\/\.]*?<\/jpc>$)/m;
+    const match = re.exec(text);
+    return match ? match[1] : text;
 }
 
 class XMLDocument {
