22621a792f7cd978a07a6319eb92aac4508d9006
Jim00000 <good0121good@gmail.com>
2021-04-17 22:03:01 +0800

    Add year

--- jpc/core/typeconverter.js
+++ jpc/core/typeconverter.js
@@ -80,7 +80,7 @@ __typeconverter.stringToGeneric = function(string) {
 
 /* MIT License
 
-Copyright (c) Jim00000
+Copyright (c) 2021 Jim00000
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
