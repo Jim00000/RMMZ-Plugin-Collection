7f9fc01cd42e4f0f1983f78c809942871c71c12d
Jim00000 <good0121good@gmail.com>
2021-04-17 22:05:06 +0800

    Add mocha config file

---
+++ .mocharc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    color: true,
+    delay: false,
+    diff: true,
+    extension: ['js'],
+    file: ['jpc/test/core/typeconverter.test.js'],
+    'full-trace': false,
+    require: 'esm',
+    ignore: [],
+    slow: '100',
+    timeout: '1000',
+    reporter: 'spec',
+    ui: 'bdd'
+};
